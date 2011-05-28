f779a87032c5d8c0b6af6f8269bb8ea1204f04a9
Spencer Kline <spencer.kline@gmail.com>
2011-05-28 11:45:04 -0400

    more sample_app stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-SampleApp::Application.configure do
+NutraNation::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   # In the development environment your application's code is reloaded on
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-SampleApp::Application.routes.draw do
+NutraNation::Application.routes.draw do
 
   resources :users do
     member do
