befbb941f887e0e62cbd4888df1f35090e542d66
Spencer Kline <skline@Spencer-Klines-MacBook-Pro.local>
2011-05-28 09:26:26 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NutraNation::Application.config.secret_token = '9ff50b7edfb99a15253d676c48c7b5b691eb9b2fc1ac3ed977a98df2127f57cd44994fd18433ee965b50ae08d71544eed5976a3cf60a7e8b0d3a75aebd0d8114'
